07d30caf49b449dbb1b030fdf867bde9c8788cf3
AniketBahukhandi <aniketbahukhandi@gmail.com>
2025-06-25 03:59:18 +0530

    fixes

--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
-import "./index.css";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+// ✅ Inject Global Styles via JavaScript
+const globalStyles = `
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Poppins', sans-serif;
+    background-color: #000;
+    color: aliceblue;
+  }
+`;
+
+// ✅ Create and append the <style> tag to the <head>
+const styleTag = document.createElement('style');
+styleTag.innerHTML = globalStyles;
+document.head.appendChild(styleTag);
+
+// ✅ Render the App
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-   <Router>
+    <Router>
       <App />
-   </Router>
+    </Router>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
